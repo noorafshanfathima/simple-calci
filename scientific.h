dbebf071358e8e1ee4958670ba8c0291992fe49e
Noor Afshan Fathima <afshan_fathima@ieee.org>
2013-07-25 18:01:59 +0530

    Create scientific.h

---
+++ scientific.h
@@ -0,0 +1,18 @@
+#ifndef SCIENTIFIC_H
+#define SCIENTIFIC_H
+
+#include <QWidget>
+
+class Scientific : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Scientific(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // SCIENTIFIC_H
